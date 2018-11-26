c1f4cfe51555e1cdc15ed7a0f4f5f8a6a666ef5c
Necrozmas <crystalfranks18@hotmail.com>
2018-11-26 18:31:23 -0500

    Update teams.js

--- features/battle/teams.js
+++ features/battle/teams.js
@@ -3,5 +3,7 @@
 */
 
 exports.teams = {
-	//Example: "tier": ['team1', 'team2']
+    "ou": [		
+      'Pinkie Pie|mew|leftovers||icebeam,willowisp,softboiled,stealthrock|Timid|240,,52,,,216|||||]Ash|greninjaash|choicespecs||hydropump,darkpulse,watershuriken,spikes|Timid|,,4,252,,252|||||]Fire Cream|charizard|charizarditex||flareblitz,dragonclaw,earthquake,roost|Jolly|,252,,,4,252|F||S||]Aggon the Kelpie|keldeoresolute|choicescarf||hydropump,secretsword,stoneedge,scald|Hasty|,,,252,4,252|||||]Twilight Sparkle|tapulele|psychiumz||psyshock,moonblast,calmmind,hiddenpowerfire|Timid|,,,252,4,252|||||]Curseyu|mimikyu|lifeorb||swordsdance,shadowsneak,playrough,shadowclaw|Adamant|,252,,,4,252|F||S||'
+	]
 };

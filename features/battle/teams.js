f91592d8f295ed5ee0d2a2818558a90022ab2a35
Necrozmas <crystalfranks18@hotmail.com>
2018-11-26 19:26:30 -0500

    Update teams.js

--- features/battle/teams.js
+++ features/battle/teams.js
@@ -3,7 +3,7 @@
 */
 
 exports.teams = {
-    "ou": [		
-      'Pinkie Pie|mew|leftovers||icebeam,willowisp,softboiled,stealthrock|Timid|240,,52,,,216|||||]Ash|greninjaash|choicespecs||hydropump,darkpulse,watershuriken,spikes|Timid|,,4,252,,252|||||]Fire Cream|charizard|charizarditex||flareblitz,dragonclaw,earthquake,roost|Jolly|,252,,,4,252|F||S||]Aggon the Kelpie|keldeoresolute|choicescarf||hydropump,secretsword,stoneedge,scald|Hasty|,,,252,4,252|||||]Twilight Sparkle|tapulele|psychiumz||psyshock,moonblast,calmmind,hiddenpowerfire|Timid|,,,252,4,252|||||]Curseyu|mimikyu|lifeorb||swordsdance,shadowsneak,playrough,shadowclaw|Adamant|,252,,,4,252|F||S||'
-	]
+	"ou": [
+			  'Bisharp||leftovers||ironhead,knockoff,suckerpunch,swordsdance|Adamant|232,252,,,4,20|||||]Chansey||eviolite||healbell,seismictoss,softboiled,thunderwave|Bold|248,,252,,8,|||||]Venusaur-Mega||venusaurite||gigadrain,hiddenpowerfire,leechseed,synthesis|Bold|240,,224,44,,||,30,,30,,30|||]Heatran||airballoon||stealthrock,fireblast,toxic,taunt|undefined|224,,,16,48,220|||||]Skarmory||rockyhelmet|1|roost,defog,spikes,bravebird|Impish|248,,232,,,28|||||]Keldeo||leftovers||scald,secretsword,substitute,calmmind|Timid|,,,252,4,252|||||'
+			  ]
 };

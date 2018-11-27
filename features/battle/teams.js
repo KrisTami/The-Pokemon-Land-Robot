83efa403e2272705137688bf83dee9f19ec1ffe7
Necrozmas <crystalfranks18@hotmail.com>
2018-11-26 20:29:09 -0500

    Update teams.js

--- features/battle/teams.js
+++ features/battle/teams.js
@@ -1,7 +1,8 @@
 /* Bot Teams */
 
 exports.teams = {
-	"gen7ou": [
-	'Pinkie Pie|mew|leftovers||icebeam,willowisp,softboiled,stealthrock|Timid|240,,52,,,216|||||]Ash|greninjaash|choicespecs||hydropump,darkpulse,watershuriken,spikes|Timid|,,4,252,,252|||||]Fire Cream|charizard|charizarditex||flareblitz,dragonclaw,earthquake,roost|Jolly|,252,,,4,252|F||S||]Aggon the Kelpie|keldeoresolute|choicescarf||hydropump,secretsword,stoneedge,scald|Hasty|,,,252,4,252|||||]Twilight Sparkle|tapulele|psychiumz||psyshock,moonblast,calmmind,hiddenpowerfire|Timid|,,,252,4,252|||||]Curseyu|mimikyu|lifeorb||swordsdance,shadowsneak,playrough,shadowclaw|Adamant|,252,,,4,252|F||S||'
-	]
+	"gen7ou": [ 'Pinkie Pie|mew|leftovers||icebeam,willowisp,softboiled,stealthrock|Timid|240,,52,,,216|||||]Ash|greninjaash|choicespecs||hydropump,darkpulse,watershuriken,spikes|Timid|,,4,252,,252|||||]Fire Cream|charizard|charizarditex||flareblitz,dragonclaw,earthquake,roost|Jolly|,252,,,4,252|F||S||]Aggon the Kelpie|keldeoresolute|choicescarf||hydropump,secretsword,stoneedge,scald|Hasty|,,,252,4,252|||||]Twilight Sparkle|tapulele|psychiumz||psyshock,moonblast,calmmind,hiddenpowerfire|Timid|,,,252,4,252|||||]Curseyu|mimikyu|lifeorb||swordsdance,shadowsneak,playrough,shadowclaw|Adamant|,252,,,4,252|F||S||'
+			  ],
+	"gen7balancedhackmons": [ 'Dragon Man|rayquazamega|lifeorb|aerilate|boomburst,fakeout,extremespeed,sunsteelstrike|Lonely|252,252,252,252,252,252|M||S||]Boost Man|arceus|safetygoggles|simple|cosmicpower,shellsmash,powertrip,batonpass|Adamant|252,252,252,,252,252|M||S||]Boost Lady|eevee|eeviumz|simple|lastresort,powertrip,batonpass,spectralthief|Lonely|252,252,252,252,252,252|F||S||]Evil Lady|gengarmega|splashplate|normalize|entrainment,shellsmash,meanlook,judgment|||F|,0,,,,|S||]Imposter Man|zygardecomplete|leftovers|magicbounce|imprison,transform,meanlook,spectralthief|Bold|252,,252,252,252,252|M||S||]Status Lady|shuckle|leftovers|prankster|haze,spectralthief,defendorder,sunsteelstrike|Careful|252,252,252,,252,252|F||||'    
+			  ]
 };

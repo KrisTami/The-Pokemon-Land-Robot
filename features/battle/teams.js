3ab6a2645a11f4f6aca78679e143cdb1ce64d15a
Necrozmas <crystalfranks18@hotmail.com>
2018-11-26 19:31:03 -0500

    Update teams.js

--- features/battle/teams.js
+++ features/battle/teams.js
@@ -3,7 +3,8 @@
 */
 
 exports.teams = {
-	"ou": [
-			  'Bisharp||leftovers||ironhead,knockoff,suckerpunch,swordsdance|Adamant|232,252,,,4,20|||||]Chansey||eviolite||healbell,seismictoss,softboiled,thunderwave|Bold|248,,252,,8,|||||]Venusaur-Mega||venusaurite||gigadrain,hiddenpowerfire,leechseed,synthesis|Bold|240,,224,44,,||,30,,30,,30|||]Heatran||airballoon||stealthrock,fireblast,toxic,taunt|undefined|224,,,16,48,220|||||]Skarmory||rockyhelmet|1|roost,defog,spikes,bravebird|Impish|248,,232,,,28|||||]Keldeo||leftovers||scald,secretsword,substitute,calmmind|Timid|,,,252,4,252|||||'
-			  ]
+	"gen7ou": [		'Bisharp||leftovers||ironhead,knockoff,suckerpunch,swordsdance|Adamant|232,252,,,4,20|||||]Chansey||eviolite||healbell,seismictoss,softboiled,thunderwave|Bold|248,,252,,8,|||||]Venusaur-Mega||venusaurite||gigadrain,hiddenpowerfire,leechseed,synthesis|Bold|240,,224,44,,||,30,,30,,30|||]Heatran||airballoon||stealthrock,fireblast,toxic,taunt|undefined|224,,,16,48,220|||||]Skarmory||rockyhelmet|1|roost,defog,spikes,bravebird|Impish|248,,232,,,28|||||]Keldeo||leftovers||scald,secretsword,substitute,calmmind|Timid|,,,252,4,252|||||',
+	'Lopunny||lopunnite|H|return,highjumpkick,substitute,encore|Jolly|,252,4,,,252|||||]Gengar||lifeorb||shadowball,sludgewave,focusblast,taunt|Timid|4,,,252,,252|||||]Garchomp||focussash|H|stealthrock,earthquake,outrage,fireblast|Naive|,252,,4,,252|||||]Starmie||leftovers|1|scald,reflecttype,recover,rapidspin|Timid|232,,24,4,,248||,0,,,,|||]Bisharp||blackglasses||knockoff,ironhead,suckerpunch,swordsdance|Adamant|4,252,,,,252|||||]Thundurus||leftovers||focusblast,thunderbolt,hiddenpowerice,thunderwave|Timid|,,4,252,,252||,30,30,,,|||', 
+	'Charizard||charizarditex||dragondance,flareblitz,roost,dragonclaw|Jolly|96,220,,,8,184|M||||]Breloom||focussash|H|spore,rocktomb,bulletseed,machpunch|Jolly|4,252,,,,252|||||]Rotom-Wash||leftovers||voltswitch,hydropump,willowisp,painsplit|Bold|252,,212,,,44|||||]Landorus-Therian||choicescarf||knockoff,earthquake,stoneedge,uturn|Jolly|,252,4,,,252|||||]Jirachi||leftovers||ironhead,wish,stealthrock,thunderwave|Jolly|252,,,,4,252|||||]Latios||lifeorb||dracometeor,earthquake,hiddenpowerfire,defog|Naive|,,16,240,,252||,30,,30,,30|||'
+	]
 };
